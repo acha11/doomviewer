7e64c67a69fef4011ede147643cdeecafd187837
andrew.chalmers <ajchalmers@gmail.com>
2020-09-13 10:29:18 +1000

    FpsStyleControls cleanup

--- src/FpsStyleControls.js
+++ src/FpsStyleControls.js
@@ -4,20 +4,18 @@ import {
 	Vector3
 } from "three";
 
-var FpsStyleControls = function ( object, domElement ) {
+// Code below was adapted from ThreeJs example "FirstPersonControls"
+var FpsStyleControls = function (object, domElement) {
+	if (domElement === undefined) {
 
-	if ( domElement === undefined ) {
-
-		console.warn( 'THREE.FpsStyleControls: The second parameter "domElement" is now mandatory.' );
+		console.warn('FpsStyleControls: The second parameter "domElement" is mandatory.');
 		domElement = document;
-
 	}
 
 	this.object = object;
 	this.domElement = domElement;
 
 	// API
-
 	this.enabled = true;
 
 	this.movementSpeed = 1.0;
@@ -40,13 +38,13 @@ var FpsStyleControls = function ( object, domElement ) {
 	this.mouseDragOn = false;
 
 	// internals
-
 	this.autoSpeedFactor = 0.0;
 
 	this.mouseX = 0;
     this.mouseY = 0;
     
     this.xVelocity = 0;
+    this.yVelocity = 0;
     this.zVelocity = 0;
 
 	this.moveForward = false;
@@ -61,7 +59,6 @@ var FpsStyleControls = function ( object, domElement ) {
 	this.viewHalfY = 0;
 
 	// private variables
-
 	var lat = 0;
 	var lon = 0;
 
@@ -69,100 +66,52 @@ var FpsStyleControls = function ( object, domElement ) {
 	var spherical = new Spherical();
 	var target = new Vector3();
 
-	//
-
-	if ( this.domElement !== document ) {
-
-		this.domElement.setAttribute( 'tabindex', - 1 );
-
+	if (this.domElement !== document) {
+		this.domElement.setAttribute('tabindex', - 1);
 	}
 
-	//
-
 	this.handleResize = function () {
-
-		if ( this.domElement === document ) {
-
+		if (this.domElement === document) {
 			this.viewHalfX = window.innerWidth / 2;
 			this.viewHalfY = window.innerHeight / 2;
-
 		} else {
-
 			this.viewHalfX = this.domElement.offsetWidth / 2;
 			this.viewHalfY = this.domElement.offsetHeight / 2;
-
 		}
-
 	};
 
-	this.onMouseDown = function ( event ) {
-
-		if ( this.domElement !== document ) {
-
+	this.onMouseDown = function (event) {
+		if (this.domElement !== document) {
 			this.domElement.focus();
-
 		}
 
 		event.preventDefault();
 		event.stopPropagation();
-
-		if ( this.activeLook ) {
-
-			switch ( event.button ) {
-
-				case 0: this.moveForward = true; break;
-				case 2: this.moveBackward = true; break;
-
-			}
-
-		}
 
 		this.mouseDragOn = true;
-
 	};
 
-	this.onMouseUp = function ( event ) {
-
+	this.onMouseUp = function (event) {
 		event.preventDefault();
 		event.stopPropagation();
 
-		if ( this.activeLook ) {
-
-			switch ( event.button ) {
-
-				case 0: this.moveForward = false; break;
-				case 2: this.moveBackward = false; break;
-
-			}
-
-		}
-
 		this.mouseDragOn = false;
-
 	};
 
-	this.onMouseMove = function ( event ) {
-
-		if ( this.domElement === document ) {
-
+	this.onMouseMove = function (event) {
+		if (this.domElement === document) {
 			this.mouseX = event.pageX - this.viewHalfX;
 			this.mouseY = event.pageY - this.viewHalfY;
-
 		} else {
-
 			this.mouseX = event.pageX - this.domElement.offsetLeft - this.viewHalfX;
 			this.mouseY = event.pageY - this.domElement.offsetTop - this.viewHalfY;
-
 		}
-
 	};
 
-	this.onKeyDown = function ( event ) {
+	this.onKeyDown = function (event) {
+		event.preventDefault();
 
-		//event.preventDefault();
-
-		switch ( event.keyCode ) {
-
+		switch (event.keyCode) {
 			case 38: /*up*/
 			case 87: /*W*/ this.moveForward = true; break;
 
@@ -179,15 +128,11 @@ var FpsStyleControls = function ( object, domElement ) {
 
 			case 82: /*R*/ this.moveUp = true; break;
 			case 70: /*F*/ this.moveDown = true; break;
-
 		}
-
 	};
 
-	this.onKeyUp = function ( event ) {
-
-		switch ( event.keyCode ) {
-
+	this.onKeyUp = function (event) {
+		switch (event.keyCode) {
 			case 38: /*up*/
 			case 87: /*W*/ this.moveForward = false; break;
 
@@ -202,160 +147,114 @@ var FpsStyleControls = function ( object, domElement ) {
 
 			case 82: /*R*/ this.moveUp = false; break;
 			case 70: /*F*/ this.moveDown = false; break;
-
 		}
-
 	};
 
-	this.lookAt = function ( x, y, z ) {
-
-		if ( x.isVector3 ) {
-
-			target.copy( x );
-
+	this.lookAt = function (x, y, z) {
+		if (x.isVector3) {
+			target.copy(x);
 		} else {
-
-			target.set( x, y, z );
-
+			target.set(x, y, z);
 		}
 
-		this.object.lookAt( target );
+		this.object.lookAt(target);
 
-		setOrientation( this );
+		setOrientation(this);
 
 		return this;
-
 	};
 
 	this.update = function () {
-
 		var targetPosition = new Vector3();
 
-		return function update( delta ) {
+		return function update(delta) {
 
-			if ( this.enabled === false ) return;
+			if (this.enabled === false) return;
 
-			if ( this.heightSpeed ) {
+			if (this.moveForward) this.zVelocity = -400;
+			if (this.moveBackward) this.zVelocity = 400;
 
-				var y = MathUtils.clamp( this.object.position.y, this.heightMin, this.heightMax );
-				var heightDelta = y - this.heightMin;
+			if (this.moveLeft) this.xVelocity = -300;
+			if (this.moveRight) this.xVelocity = 300;
 
-				this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
-
-			} else {
-
-				this.autoSpeedFactor = 0.0;
-
-			}
-
-			var actualMoveSpeed = delta * this.movementSpeed;
-
-			if ( this.moveForward) this.zVelocity = -400;
-			if ( this.moveBackward) this.zVelocity = 400;
-
-			if ( this.moveLeft ) this.xVelocity = -300;
-			if ( this.moveRight ) this.xVelocity = 300;
-
-			if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
-			if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
-
+			if (this.moveDown) this.yVelocity = -200;
+			if (this.moveUp) this.yVelocity = 200;
 
             this.object.translateX(this.xVelocity * delta);
+            this.object.translateY(this.yVelocity * delta);
             this.object.translateZ(this.zVelocity * delta);
 
             this.xVelocity *= 0.85;
+            this.yVelocity *= 0.85;
             this.zVelocity *= 0.85;
-
-			var actualLookSpeed = delta * this.lookSpeed;
-
-			if ( ! this.activeLook ) {
-				actualLookSpeed = 0;
-			}
-
-			var verticalLookRatio = 1;
-
-			if ( this.constrainVertical ) {
-
-				verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
-
-			}
 
             if (this.turnLeft) lon += delta * 150;
             if (this.turnRight) lon -= delta * 150;
-            
-            lat = 0; // Always look straight level forwards - not up and down
 
-			var phi = MathUtils.degToRad( 90 - lat );
-			var theta = MathUtils.degToRad( lon );
+            // Code below calculates polar coordinates for the look direction based on lat and lon,
+            // then calculates a targetPosition in world space that the camera should look at
+            // accordingly, then tells the camera to look at that location.
+            
+            lat = 0; // Always look out at the horizon - not angled up or down
+
+			var phi = MathUtils.degToRad(90 - lat);
+			var theta = MathUtils.degToRad(lon);
 
 			var position = this.object.position;
 
-			targetPosition.setFromSphericalCoords( 1, phi, theta ).add( position );
+			targetPosition.setFromSphericalCoords(1, phi, theta).add(position);
 
-			this.object.lookAt( targetPosition );
-
+			this.object.lookAt(targetPosition);
 		};
-
 	}();
 
-	function contextmenu( event ) {
-
+	function contextmenu(event) {
 		event.preventDefault();
-
 	}
 
 	this.dispose = function () {
+		this.domElement.removeEventListener('contextmenu', contextmenu, false);
+		this.domElement.removeEventListener('mousedown', _onMouseDown, false);
+		this.domElement.removeEventListener('mousemove', _onMouseMove, false);
+		this.domElement.removeEventListener('mouseup', _onMouseUp, false);
 
-		this.domElement.removeEventListener( 'contextmenu', contextmenu, false );
-		this.domElement.removeEventListener( 'mousedown', _onMouseDown, false );
-		this.domElement.removeEventListener( 'mousemove', _onMouseMove, false );
-		this.domElement.removeEventListener( 'mouseup', _onMouseUp, false );
-
-		window.removeEventListener( 'keydown', _onKeyDown, false );
-		window.removeEventListener( 'keyup', _onKeyUp, false );
-
+		window.removeEventListener('keydown', _onKeyDown, false);
+		window.removeEventListener('keyup', _onKeyUp, false);
 	};
 
-	var _onMouseMove = bind( this, this.onMouseMove );
-	var _onMouseDown = bind( this, this.onMouseDown );
-	var _onMouseUp = bind( this, this.onMouseUp );
-	var _onKeyDown = bind( this, this.onKeyDown );
-	var _onKeyUp = bind( this, this.onKeyUp );
+	var _onMouseMove = bind(this, this.onMouseMove);
+	var _onMouseDown = bind(this, this.onMouseDown);
+	var _onMouseUp = bind(this, this.onMouseUp);
+	var _onKeyDown = bind(this, this.onKeyDown);
+	var _onKeyUp = bind(this, this.onKeyUp);
 
-	this.domElement.addEventListener( 'contextmenu', contextmenu, false );
-	this.domElement.addEventListener( 'mousemove', _onMouseMove, false );
-	this.domElement.addEventListener( 'mousedown', _onMouseDown, false );
-	this.domElement.addEventListener( 'mouseup', _onMouseUp, false );
+	this.domElement.addEventListener('contextmenu', contextmenu, false);
+	this.domElement.addEventListener('mousemove', _onMouseMove, false);
+	this.domElement.addEventListener('mousedown', _onMouseDown, false);
+	this.domElement.addEventListener('mouseup', _onMouseUp, false);
 
-	window.addEventListener( 'keydown', _onKeyDown, false );
-	window.addEventListener( 'keyup', _onKeyUp, false );
+	window.addEventListener('keydown', _onKeyDown, false);
+	window.addEventListener('keyup', _onKeyUp, false);
 
-	function bind( scope, fn ) {
-
+	function bind(scope, fn) {
 		return function () {
-
-			fn.apply( scope, arguments );
-
+			fn.apply(scope, arguments);
 		};
-
 	}
 
-	function setOrientation( controls ) {
-
+	function setOrientation(controls) {
 		var quaternion = controls.object.quaternion;
 
-		lookDirection.set( 0, 0, - 1 ).applyQuaternion( quaternion );
-		spherical.setFromVector3( lookDirection );
+		lookDirection.set(0, 0, - 1).applyQuaternion(quaternion);
+		spherical.setFromVector3(lookDirection);
 
-		lat = 90 - MathUtils.radToDeg( spherical.phi );
-		lon = MathUtils.radToDeg( spherical.theta );
-
+		lat = 90 - MathUtils.radToDeg(spherical.phi);
+		lon = MathUtils.radToDeg(spherical.theta);
 	}
 
 	this.handleResize();
 
-	setOrientation( this );
-
+	setOrientation(this);
 };
 
 export { FpsStyleControls };
